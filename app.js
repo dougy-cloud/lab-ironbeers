4df972de00ebaa53a3da64f4a7b4ab0f77e40582
Dougy <dougycloud@gmail.com>
2022-08-10 08:16:29 +0200

    solved without bonuses

--- app.js
+++ app.js
@@ -22,4 +22,10 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.listen(3000, () => console.log('🏃‍ on port 3000'));
+app.listen(3005, () => console.log('🏃‍ on http://localhost:3005'));
+
+app.get("/beers", (req, res) => {
+  punkAPI.getBeers()
+  .then(beersFromAPI => res.render('beers', {beersFromApi}))
+  .catch(error => console.log(error))
+});
